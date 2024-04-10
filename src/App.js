6333f05cc94b4e06e59caf320704ba383f5fcfe4
Harkesh <harkesh.sain@gmail.com>
2024-04-10 22:22:20 +0530

    Base Register Page Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import GettingStarted from "./components/GettingStarted";
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Coming Soon, Stay Tuned</p>
-            </header>
+            <GettingStarted />
         </div>
     );
 }
---
+++ src/components/GettingStarted.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import logo from "../logo.svg";
+import Button from "@mui/material/Button";
+
+function MyComponent() {
+    return (
+        <Grid
+            container
+            spacing={2}
+            sx={{
+                height: "100vh",
+                backgroundImage: `url('/staticassets/images/register_bg.jpeg')`,
+                backgroundPosition:'bottom center'
+            }}
+        >
+            <Grid
+                item
+                xs={12}
+                sm={4}
+                md={4}
+                lg={4}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                textAlign="center"
+                direction={"column"}
+            >
+                <img src={logo} className="App-logo" alt="logo" />
+                <Typography variant="h3" color="red">
+                    Coming Soon
+                </Typography>
+                <Typography variant="h5" color="red">
+                    Stay Tuned
+                </Typography>
+
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    sx={{ mt: 10, width: "70%", borderRadius: "50px" }}
+                    size="large"
+                >
+                    Register Now
+                </Button>
+                <Typography variant="body1" color="white">
+                    Already registered, <a href="pathtologincomponent">Login</a>
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default MyComponent;
