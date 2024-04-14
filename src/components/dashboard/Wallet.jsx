eebab6ce647cac7b1b0f260aa4c15f2bce6b78db
Harkesh <harkesh.sain@gmail.com>
2024-04-14 12:50:27 +0530

    earnings added

--- src/components/dashboard/Wallet.jsx
+++ src/components/dashboard/Wallet.jsx
@@ -29,9 +29,9 @@ function Wallet() {
                     style={{ height: "100%" }}
                 >
                     <Grid item>
-                        <Grid container direction="column" spacing={1}>
+                        <Grid container direction="column" spacing={0}>
                             <Grid item>
-                                <Typography variant="h5" color={"white"}>
+                                <Typography variant="body1" color={"white"}>
                                     Total Balance
                                 </Typography>
                             </Grid>
@@ -52,13 +52,176 @@ function Wallet() {
                             <Grid item>
                                 <Chip
                                     label="100% Secure payments"
-                                    color="info"
+                                    color="success"
+                                    size="medium"
+                                    sx={{ width: "100%" }}
                                 />
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
             </Paper>
+            <Grid container spacing={2} justifyContent={"center"} mt={2}>
+                <Grid item>
+                    {/* Deposit Paper */}
+                    <Paper
+                        sx={{
+                            padding: 2,
+                            borderRadius: 5,
+                            borderColor: "white",
+                            backgroundColor: (theme) =>
+                                alpha(theme.palette.grey[500], 0.5),
+                            width: "95%", // Adjust width to 95%
+                            textAlign: "center", // Align center horizontally
+                            margin: "auto", // Center horizontally
+                        }}
+                    >
+                        <Grid
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                            style={{ height: "100%" }}
+                        >
+                            <Grid item>
+                                <Grid container direction="column" spacing={0}>
+                                    <Grid item>
+                                        <Typography
+                                            variant="h4"
+                                            component="span"
+                                        >
+                                            <span
+                                                style={{
+                                                    fontSize: "0.5em",
+                                                    verticalAlign: "super",
+                                                }}
+                                            >
+                                                ₹
+                                            </span>
+                                            0
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid item>
+                                        <Chip
+                                            label="Deposit"
+                                            color="secondary"
+                                            size="small"
+                                            sx={{ width: "100%" }}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    {/* Deposit Paper */}
+                </Grid>
+                <Grid item>
+                    {/* Earnings */}
+                    <Paper
+                        sx={{
+                            padding: 2,
+                            borderRadius: 5,
+                            borderColor: "white",
+                            backgroundColor: (theme) =>
+                                alpha(theme.palette.grey[500], 0.5),
+                            width: "95%", // Adjust width to 95%
+                            textAlign: "center", // Align center horizontally
+                            margin: "auto", // Center horizontally
+                        }}
+                    >
+                        <Grid
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                            style={{ height: "100%" }}
+                        >
+                            <Grid item>
+                                <Grid container direction="column" spacing={0}>
+                                    <Grid item>
+                                        <Typography
+                                            variant="h4"
+                                            component="span"
+                                        >
+                                            <span
+                                                style={{
+                                                    fontSize: "0.5em",
+                                                    verticalAlign: "super",
+                                                }}
+                                            >
+                                                ₹
+                                            </span>
+                                            0
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid item>
+                                        <Chip
+                                            label="Earnings"
+                                            color="secondary"
+                                            size="small"
+                                            sx={{ width: "100%" }}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    {/*  Earnings */}
+                </Grid>
+                <Grid item>
+                    {/* Commission */}
+                    <Paper
+                        sx={{
+                            padding: 2,
+                            borderRadius: 5,
+                            borderColor: "white",
+                            backgroundColor: (theme) =>
+                                alpha(theme.palette.grey[500], 0.5),
+                            width: "95%", // Adjust width to 95%
+                            textAlign: "center", // Align center horizontally
+                            margin: "auto", // Center horizontally
+                        }}
+                    >
+                        <Grid
+                            container
+                            justifyContent="center"
+                            alignItems="center"
+                            style={{ height: "100%" }}
+                        >
+                            <Grid item>
+                                <Grid container direction="column" spacing={0}>
+                                    <Grid item>
+                                        <Typography
+                                            variant="h4"
+                                            component="span"
+                                        >
+                                            <span
+                                                style={{
+                                                    fontSize: "0.5em",
+                                                    verticalAlign: "super",
+                                                }}
+                                            >
+                                                ₹
+                                            </span>
+                                            0
+                                        </Typography>
+                                    </Grid>
+
+                                    <Grid item>
+                                        <Chip
+                                            label="Commission"
+                                            color="secondary"
+                                            size="small"
+                                            sx={{ width: "100%" }}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    {/* Commission */}
+                </Grid>
+            </Grid>
         </Layout>
     );
 }
