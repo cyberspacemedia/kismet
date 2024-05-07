fa3f8624f1ddad6302a8b30f0b972c4e5feb60f8
Harkesh <harkesh.sain@gmail.com>
2024-05-07 20:50:38 +0530

    Leaderboard

--- src/components/dashboard/Appcomp/Leaderboard.jsx
+++ src/components/dashboard/Appcomp/Leaderboard.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Layout from "../Layout";
 import Typography from "@mui/material/Typography";
+import { Box, Grid, Paper } from "@mui/material";
 
 function Leaderboard() {
     return (
         <Layout>
             <Typography variant="caption"> Leader Board</Typography>
+            <Box sx={{ width: "350px" }}>
+                <Paper
+                    variant="outlined"
+                    sx={{
+                        padding: 2,
+                        borderRadius: 2,
+                       
+                        background:
+                            "linear-gradient(90deg, rgba(238,208,65,1) 0%, rgba(255,147,0,1) 100%)",
+
+                        textAlign: "center", // Align center horizontally
+                        margin: "5px auto 0", // Center horizontally
+                    }}
+                >
+                    <Grid container>
+                        <Grid item xs={4}>
+                            1
+                        </Grid>
+                        <Grid item xs={4}>
+                            Rahul 
+                        </Grid>
+                        <Grid item xs={4}>
+                            1,23,984
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Box>
         </Layout>
     );
 }
