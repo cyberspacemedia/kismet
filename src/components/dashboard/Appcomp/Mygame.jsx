d7d2598ab34f76a8cbc486bbc2034f59e312dc79
Harkesh <harkesh.sain@gmail.com>
2024-05-14 17:36:37 +0530

    Started my Game

--- src/components/dashboard/Appcomp/Mygame.jsx
+++ src/components/dashboard/Appcomp/Mygame.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../Layout";
 import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
 
 function Mygame() {
     return (
         <Layout>
             <Typography variant="caption"> My Game</Typography>
+            <Grid container>
+                <Grid item>Item 1</Grid>
+            </Grid>
         </Layout>
     );
 }
