cfd0da2c31df3fdd97df9226033c8e0e5535c40e
Harkesh <harkesh.sain@gmail.com>
2024-05-14 18:45:21 +0530

    mygame

--- src/components/dashboard/Appcomp/Mygame.jsx
+++ src/components/dashboard/Appcomp/Mygame.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Layout from "../Layout";
 import Typography from "@mui/material/Typography";
-import { Grid } from "@mui/material";
+import { Grid, CardHeader, Avatar, IconButton, Card } from "@mui/material";
 
 function Mygame() {
     return (
         <Layout>
             <Typography variant="caption"> My Game</Typography>
-            <Grid container>
-                <Grid item>Item 2</Grid>
+            <Grid container justifyContent="center">
+                <Grid item xs={12} md={10}>
+                    <Card sx={{ width: "90%", margin: "0 auto" }}>
+                        <CardHeader
+                            avatar={<Avatar aria-label=""></Avatar>}
+                            action={<IconButton aria-label=""></IconButton>}
+                            title="Game Name"
+                            subheader="Game Status"
+                        />
+                    </Card>
+                </Grid>
             </Grid>
         </Layout>
     );
