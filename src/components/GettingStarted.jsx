82c31e6c04ced88c9505f341aac3f5536b5b6ca7
Harkesh <harkesh.sain@gmail.com>
2024-05-22 11:50:11 +0530

    Getting Started Fixed to center

--- src/components/GettingStarted.jsx
+++ src/components/GettingStarted.jsx
@@ -15,6 +15,9 @@ function GettingStarted() {
     return (
         <Grid
             container
+            direction={"column"}
+            display="flex"
+            textAlign="center"
             sx={{
                 height: "100vh",
                 backgroundImage: `url('./StaticAssets/Images/Getting_Started_wologo.jpg')`,
@@ -23,63 +26,43 @@ function GettingStarted() {
                 backgroundColor: "black",
             }}
         >
-            <Grid
-                item
-                xs={12}
-                sm={4}
-                md={4}
-                lg={4}
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                textAlign="center"
-                flexDirection="column" // Display items below each other
-            >
-                <img
-                    src="./StaticAssets/Images/moblogo.png"
-                    alt="Logo"
-                    style={{ height: "150px" }}
-                />
-                <Typography variant="body1" sx={{ mt: 5 }}>
-                    Welcome to
-                </Typography>
-                <Typography variant="h4">KISMET APP</Typography>
-            </Grid>
-            <Grid
-                item
-                xs={12}
-                sm={4}
-                md={4}
-                lg={4}
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                textAlign="center"
-                flexDirection="column" // Display items below each other
-            >
-                <Button
-                    variant="contained"
-                    sx={{ mt: 10, ...theme.buttons.gradient }}
-                    size="large"
-                    onClick={handleRegister}
+            <Box xs={12} sm={4} md={4} lg={4}>
+                <Grid item sx={{ mt: 5 }}>
+                    <img
+                        src="./StaticAssets/Images/moblogo.png"
+                        alt="Logo"
+                        style={{ height: "150px" }}
+                    />
+                    <Typography variant="body1" sx={{ mt: 3 }}>
+                        Welcome to
+                    </Typography>
+                    <Typography variant="h4">KISMET APP</Typography>
+                </Grid>
+                <Grid item sx={{ width: "80%", margin: "auto" }}>
+                    <Button
+                        variant="contained"
+                        sx={{ mt: 5, ...theme.buttons.gradient }}
+                        size="large"
+                        onClick={handleRegister}
+                    >
+                        Register Now
+                    </Button>
+                    <Typography variant="body1" color="white" sx={{ mt: 2 }}>
+                        Already registered, <Link to="/login">Login</Link>
+                    </Typography>
+                </Grid>
+
+                <Box
+                    sx={{
+                        position: "absolute",
+                        bottom: 1,
+                        right: 1,
+                    }}
                 >
-                    Register Now
-                </Button>
-                <Typography variant="body1" color="white" sx={{ mt: 2 }}>
-                    Already registered, <Link to="/login">Login</Link>
-                </Typography>
-            </Grid>
-            <Box
-                sx={{
-                    position: "absolute",
-                    bottom: 1,
-                    right: 1,
-                    // Add padding to create some space
-                }}
-            >
-                <Typography variant="caption" color="white">
-                    V1.0
-                </Typography>
+                    <Typography variant="caption" color="white">
+                        Ver 1.10
+                    </Typography>
+                </Box>
             </Box>
         </Grid>
     );
