c0d300e4a2efc56e47fd3b486cb7eb3697447847
Harkesh <harkesh.sain@gmail.com>
2024-05-22 15:09:23 +0530

    Gururam Game started with Test Screen

--- src/components/Games/Gurugramgame.jsx
+++ src/components/Games/Gurugramgame.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "../dashboard/Layout";
 import { Chip, Grid } from "@mui/material";
+import Test from "../Test";
 function Gurugramgame() {
     return (
         <Layout>
-            <Grid container>
-                <Grid item xs={12} mt={20}>
-                    <Chip label="Coming Soon" size="medium" color="warning" />
+            <Grid container spacing={1}>
+                <Grid item xs={12} md={12}>
+                    <Chip label="Gurugram" size="small" color="warning" />
+                </Grid>
+                <Grid item xs={12} md={12}>
+                    <Test />
                 </Grid>
             </Grid>
         </Layout>
