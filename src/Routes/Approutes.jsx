0fcdf37f7eaad0d5ee641f02dadb8714bb58ffbe
Harkesh <harkesh.sain@gmail.com>
2024-06-16 21:30:30 +0530

    All Restored

--- src/Routes/Approutes.jsx
+++ src/Routes/Approutes.jsx
@@ -20,7 +20,6 @@ import Rohtakgameinfo from "../components/Games/Rohtakgameinfo";
 import Rewarigameinfo from "../components/Games/Rewarigameinfo";
 import Alwargameinfo from "../components/Games/Alwargameinfo";
 import MainGame from "../components/Roulette/MainGame";
-import Layout from "../components/dashboard/Layout";
 
 function Approutes() {
     const isLoggedIn = false; // Set it based on your authentication state
@@ -28,7 +27,6 @@ function Approutes() {
     return (
         <Routes>
             <Route path="/test" element={<Test />} />
-            <Route path="/" element={<Layout />} />
             <Route path="/dashboard/*" element={<Dashboard />} />
             <Route path="/gurugramgame/*" element={<Gurugramgame />} />
 
