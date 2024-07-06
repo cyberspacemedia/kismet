a8984dcaae777e6199650e71fe87383b1823d764
Harkesh <harkesh.sain@gmail.com>
2024-07-06 14:25:13 +0530

    Top Row Completed

--- src/components/Games/Allgames.jsx
+++ src/components/Games/Allgames.jsx
@@ -6,13 +6,24 @@ import {
     CardContent,
     CardMedia,
     Typography,
+    useMediaQuery,
 } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+
 function Allgames() {
     const navigate = useNavigate()
-
+    const isSmallScreen = useMediaQuery('(max-width:600px)')
     return (
-        <Grid container spacing={1} p={2} sx={{ backgroundColor: '#110b19' }}>
+        <Grid
+            container
+            spacing={1}
+            p={2}
+            sx={{
+                backgroundColor: '#110b19',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
             <Grid item xs={3}>
                 {/* Card Disawar */}
                 <Card
@@ -21,8 +32,8 @@ function Allgames() {
                             'linear-gradient(40deg, rgba(123,0,180,1) 0%, rgba(255,0,104,1) 100%)',
                         borderRadius: '15px', // Optional: Add border radius if needed
                         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', // Optional: Add shadow for a nicer effect
-                        height: '3em',
-                        width: '3em',
+                        height: isSmallScreen ? '3em' : '8em', // Responsive height
+                        width: isSmallScreen ? '3em' : '8em', // Responsive width
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
@@ -66,11 +77,11 @@ function Allgames() {
                 <Card
                     sx={{
                         background:
-                            'linear-gradient(47deg, rgba(176,10,252,1) 0%, rgba(255,0,138,1) 100%)',
+                            'linear-gradient(40deg, rgba(123,0,180,1) 0%, rgba(255,0,104,1) 100%)',
                         borderRadius: '15px', // Optional: Add border radius if needed
                         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', // Optional: Add shadow for a nicer effect
-                        height: '3em',
-                        width: '3em',
+                        height: isSmallScreen ? '3em' : '8em', // Responsive height
+                        width: isSmallScreen ? '3em' : '8em', // Responsive width
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
@@ -110,36 +121,100 @@ function Allgames() {
                 {/* End Card Gali */}
             </Grid>
             <Grid item xs={3}>
-                {/* Card 1 */}
-                <Card sx={{ backgroundColor: 'transparent' }}>
+                {/* Card Faridabad */}
+                <Card
+                    sx={{
+                        background:
+                            'linear-gradient(40deg, rgba(123,0,180,1) 0%, rgba(255,0,104,1) 100%)',
+                        borderRadius: '15px', // Optional: Add border radius if needed
+                        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', // Optional: Add shadow for a nicer effect
+                        height: isSmallScreen ? '3em' : '8em', // Responsive height
+                        width: isSmallScreen ? '3em' : '8em', // Responsive width
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
                     <CardActionArea
                         onClick={() => navigate('/faridabadgameinfo')}
+                        sx={{ height: '100%', width: '100%' }}
                     >
                         <CardMedia
                             component="img"
-                            image="./StaticAssets/Images/Icons/Gurugram.png"
-                            alt="Gurugram"
+                            image="./StaticAssets/Images/Icons/Faridabad.png"
+                            alt="Disawar"
+                            sx={{
+                                height: '50%', // Adjust the height to fit the card
+                                width: '50%', // Adjust the width to fit the card
+                                objectFit: 'cover', // Cover the card area
+                                margin: 'auto',
+                            }}
                         />
+                        <CardContent
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                padding: '8px 0px 0px 0px', // Padding to adjust the spacing
+                            }}
+                        >
+                            <Typography variant="h6" sx={{ color: 'white' }}>
+                                Faridabad
+                            </Typography>
+                        </CardContent>
                     </CardActionArea>
                 </Card>
-                <Typography variant="h6">Faridabad</Typography>
-                {/* Card 1 */}
+                {/* End Card Faridabad */}
             </Grid>
             <Grid item xs={3}>
-                {/* Card 1 */}
-                <Card sx={{ backgroundColor: 'transparent' }}>
+                {/* Card Gaziabad */}
+                <Card
+                    sx={{
+                        background:
+                            'linear-gradient(40deg, rgba(123,0,180,1) 0%, rgba(255,0,104,1) 100%)',
+                        borderRadius: '15px', // Optional: Add border radius if needed
+                        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)', // Optional: Add shadow for a nicer effect
+                        height: isSmallScreen ? '3em' : '8em', // Responsive height
+                        width: isSmallScreen ? '3em' : '8em', // Responsive width
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
                     <CardActionArea
                         onClick={() => navigate('/gaziabadgameinfo')}
+                        sx={{ height: '100%', width: '100%' }}
                     >
                         <CardMedia
                             component="img"
-                            image="./StaticAssets/Images/Icons/Gurugram.png"
-                            alt="Gurugram"
+                            image="./StaticAssets/Images/Icons/Gaziabad.png"
+                            alt="Disawar"
+                            sx={{
+                                height: '50%', // Adjust the height to fit the card
+                                width: '50%', // Adjust the width to fit the card
+                                objectFit: 'cover', // Cover the card area
+                                margin: 'auto',
+                            }}
                         />
+                        <CardContent
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                padding: '8px 0px 0px 0px', // Padding to adjust the spacing
+                            }}
+                        >
+                            <Typography variant="h6" sx={{ color: 'white' }}>
+                                Gaziabad
+                            </Typography>
+                        </CardContent>
                     </CardActionArea>
                 </Card>
-                <Typography variant="h6">Gaziabad</Typography>
-                {/* Card 1 */}
+                {/* End Card Gaziabad */}
             </Grid>
 
             <Grid item xs={3}>
