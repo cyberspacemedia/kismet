b701beab5bb61e320328f57d405048358338cb77
Harkesh <harkesh.sain@gmail.com>
2024-07-27 12:12:22 +0530

    Account History Pending

--- src/components/dashboard/Accounthistory.jsx
+++ src/components/dashboard/Accounthistory.jsx
@@ -1,323 +1,74 @@
-import React from "react";
-import Layout from "./Layout";
-import { Box, Typography, Grid, Chip, Paper } from "@mui/material";
-import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
-function Accounthistory() {
+import React from 'react'
+import Topmenu from './Appcomp/Topmenu'
+import BottomMenu from './Appcomp/Bottommenu'
+import { Box, Chip, Grid, Icon, Typography } from '@mui/material'
+function CustomGridItem({ icon, status, amount, datetime, txnId }) {
     return (
-        <Layout>
-            <Box sx={{ m: 2, textAlign: "center" }}>
-                <Typography variant="h6" color={"#ffffff"}>
-                    Transaction History
-                </Typography>
-            </Box>
-            <Box
-                mt={1}
-                sx={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                }}
+        <Box
+            sx={{ p: 2, border: '1px solid #ccc', borderRadius: '5px', mb: 2 }}
+        >
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
             >
-                <Paper
-                    square
-                    sx={{
-                        backgroundColor: "#3D3D3D",
-                        width: "90%",
-                        padding: "10px",
-                    }}
-                >
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <Grid container>
-                                <Grid item xs={2} textAlign={"center"}>
-                                    <AccountBalanceWalletIcon />
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography variant="caption">
-                                        Status:{" "}
-                                    </Typography>
-                                    <span
-                                        style={{
-                                            fontSize: "16px",
-                                            color: "green",
-                                        }}
-                                    >
-                                        Success
-                                    </span>
-                                </Grid>
-                                <Grid item xs={5} textAlign={"end"}>
-                                    <Chip
-                                        label="Deposit"
-                                        color="success"
-                                        size="small"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sx={{ textAlign: "center" }}>
-                            <Grid
-                                container
-                                direction="column"
-                                alignItems="flex-end"
-                            >
-                                <Grid item>
-                                    <Typography variant="h6" color={"wheat"}>
-                                        ₹ 300
-                                    </Typography>
-                                </Grid>
-                                <Typography variant="caption">
-                                    07-Apr-24 10:46 AM Tue
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid
-                            item
-                            xs={12}
-                            mt={2}
-                            sx={{ textAlign: "center", alignItems: "center" }}
-                        >
-                            <Chip
-                                label="Transaction ID : 3976XFFF378C"
-                                variant="outlined"
-                                size="small"
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Box>
-
-            <Box
-                mt={1}
-                sx={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                }}
-            >
-                <Paper
-                    square
-                    sx={{
-                        backgroundColor: "#3D3D3D",
-                        width: "90%",
-                        padding: "10px",
-                    }}
-                >
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <Grid container>
-                                <Grid item xs={2} textAlign={"center"}>
-                                    <AccountBalanceWalletIcon />
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography variant="caption">
-                                        Status:{" "}
-                                    </Typography>
-                                    <span
-                                        style={{
-                                            fontSize: "16px",
-                                            color: "green",
-                                        }}
-                                    >
-                                        Success
-                                    </span>
-                                </Grid>
-                                <Grid item xs={5} textAlign={"end"}>
-                                    <Chip
-                                        label="Deposit"
-                                        color="success"
-                                        size="small"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sx={{ textAlign: "center" }}>
-                            <Grid
-                                container
-                                direction="column"
-                                alignItems="flex-end"
-                            >
-                                <Grid item>
-                                    <Typography variant="h6" color={"wheat"}>
-                                        ₹ 300
-                                    </Typography>
-                                </Grid>
-                                <Typography variant="caption">
-                                    07-Apr-24 10:46 AM Tue
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid
-                            item
-                            xs={12}
-                            mt={2}
-                            sx={{ textAlign: "center", alignItems: "center" }}
-                        >
-                            <Chip
-                                label="Transaction ID : 3976XFFF378C"
-                                variant="outlined"
-                                size="small"
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Box>
-            <Box
-                mt={1}
-                sx={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                }}
-            >
-                <Paper
-                    square
-                    sx={{
-                        backgroundColor: "#3D3D3D",
-                        width: "90%",
-                        padding: "10px",
-                    }}
-                >
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <Grid container>
-                                <Grid item xs={2} textAlign={"center"}>
-                                    <AccountBalanceWalletIcon />
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography variant="caption">
-                                        Status:{" "}
-                                    </Typography>
-                                    <span
-                                        style={{
-                                            fontSize: "16px",
-                                            color: "green",
-                                        }}
-                                    >
-                                        Success
-                                    </span>
-                                </Grid>
-                                <Grid item xs={5} textAlign={"end"}>
-                                    <Chip
-                                        label="Withdraw"
-                                        color="warning"
-                                        size="small"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sx={{ textAlign: "center" }}>
-                            <Grid
-                                container
-                                direction="column"
-                                alignItems="flex-end"
-                            >
-                                <Grid item>
-                                    <Typography variant="h6" color={"wheat"}>
-                                        ₹ 1300
-                                    </Typography>
-                                </Grid>
-                                <Typography variant="caption">
-                                    07-Apr-24 10:46 AM Tue
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid
-                            item
-                            xs={12}
-                            mt={2}
-                            sx={{ textAlign: "center", alignItems: "center" }}
-                        >
-                            <Chip
-                                label="Transaction ID : 3976XFFF378C"
-                                variant="outlined"
-                                size="small"
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Box>
-
-            <Box
-            mt={1}
-                sx={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    
-                }}
-            >
-                <Paper
-                    square
-                    sx={{
-                        backgroundColor: "#3D3D3D",
-                        width: "90%",
-                        padding: "10px",
-                    }}
-                >
-                    <Grid container>
-                        <Grid item xs={12}>
-                            <Grid container>
-                                <Grid item xs={2} textAlign={"center"}>
-                                    <AccountBalanceWalletIcon />
-                                </Grid>
-                                <Grid item xs={5}>
-                                    <Typography variant="caption">
-                                        Status:{" "}
-                                    </Typography>
-                                    <span
-                                        style={{
-                                            fontSize: "16px",
-                                            color: "green",
-                                        }}
-                                    >
-                                        Success
-                                    </span>
-                                </Grid>
-                                <Grid item xs={5} textAlign={"end"}>
-                                    <Chip
-                                        label="Deposit"
-                                        color="success"
-                                        size="small"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item xs={12} sx={{ textAlign: "center" }}>
-                            <Grid
-                                container
-                                direction="column"
-                                alignItems="flex-end"
-                            >
-                                <Grid item>
-                                    <Typography variant="h6" color={"wheat"}>
-                                        ₹ 300
-                                    </Typography>
-                                </Grid>
-                                <Typography variant="caption">
-                                    07-Apr-24 10:46 AM Tue
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid
-                            item
-                            xs={12}
-                            mt={2}
-                            sx={{ textAlign: "center", alignItems: "center" }}
-                        >
-                            <Chip
-                                label="Transaction ID : 3976XFFF378C"
-                                variant="outlined"
-                                size="small"
-                            />
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Box>
-
-
-            
-        </Layout>
-    );
+                <Grid item>
+                    <Box display="flex" alignItems="center">
+                        <Icon>{icon}</Icon>
+                    </Box>
+                </Grid>
+                <Grid item xs>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                    >
+                        <Typography variant="body1">{status}</Typography>
+                        <Chip
+                            label={`Txn ID: ${txnId}`}
+                            color="primary"
+                            sx={{ mt: 1 }}
+                        />
+                    </Box>
+                </Grid>
+                <Grid item>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="flex-end"
+                    >
+                        <Typography variant="body2">{amount}</Typography>
+                        <Typography variant="body2">{datetime}</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    )
 }
+export default function Accounthistory() {
+    return (
+        <div className="layout-container">
+            <div className="top-menu">
+                <Topmenu menu="TRANSACTIONS" />
+            </div>
 
-export default Accounthistory;
+            <div className="content">
+                <Grid container direction="column" justifyContent="center">
+                    <Grid item>
+                        <CustomGridItem
+                           
+                            status="Completed"
+                            amount="$120"
+                            datetime="2024-07-25 14:35"
+                            txnId="1234567890"
+                        />
+                    </Grid>
+                </Grid>
+            </div>
+            <div className="bottom-menu">
+                <BottomMenu />
+            </div>
+        </div>
+    )
+}
