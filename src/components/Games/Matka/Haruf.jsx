4b99160ed23133a91288ebb0e6b7c92e806b10ae
Harkesh <harkesh.sain@gmail.com>
2024-07-30 14:40:40 +0530

    Haruf Submitted

--- src/components/Games/Matka/Haruf.jsx
+++ src/components/Games/Matka/Haruf.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Box, Grid, TextField, Typography, Button } from '@mui/material'
+import UserContext from '../../UserContext'
+import theme from '../../../theme/Theme'
+import { apiClient } from '../../config/Config'
 
-function Haruf() {
+function Haruf({ gameName, gameId, gameType }) {
+    const { userId } = useContext(UserContext)
     const numbers = Array.from({ length: 10 }, (_, index) =>
         index.toString().padStart(2, '0')
     )
@@ -9,6 +13,17 @@ function Haruf() {
     const [inputValues, setInputValues] = useState({
         A: Array(10).fill(''),
         B: Array(10).fill(''),
+    })
+
+    const [betData, setBetData] = useState({
+        userId: userId,
+        gameName: gameName,
+        gameType: gameType,
+        gameId: gameId,
+        list: {
+            A: [],
+            B: [],
+        },
     })
 
     const handleChange = (game, index, value) => {
@@ -24,7 +39,6 @@ function Haruf() {
             A: [],
             B: [],
         }
-
         ;['A', 'B'].forEach((game) => {
             inputValues[game].forEach((amount, index) => {
                 if (amount) {
@@ -39,7 +53,21 @@ function Haruf() {
         if (list.A.length === 0 && list.B.length === 0) {
             console.log('Validation: Data not found')
         } else {
-            console.log('Commision History:', list)
+            const updatedBetData = {
+                ...betData,
+                list: list,
+            }
+            setBetData(updatedBetData)
+            console.log('Commision History:', updatedBetData)
+        }
+    }
+
+    const handleSubmit = async () => {
+        try {
+            const response = await apiClient.post('/submitgame3', betData)
+            console.log(response)
+        } catch (error) {
+            console.error('API error Submit Haruf Game', error)
         }
     }
 
@@ -83,7 +111,7 @@ function Haruf() {
                                         variant="outlined"
                                         size="small"
                                         margin="dense"
-                                        placeholder="00"
+                                        placeholder="₹00"
                                         type="number"
                                         value={inputValues.A[index]}
                                         onChange={(e) =>
@@ -143,7 +171,7 @@ function Haruf() {
                                         variant="outlined"
                                         size="small"
                                         margin="dense"
-                                        placeholder="00"
+                                        placeholder="₹00"
                                         type="number"
                                         value={inputValues.B[index]}
                                         onChange={(e) =>
@@ -181,6 +209,45 @@ function Haruf() {
                         >
                             Save
                         </Button>
+                    </Grid>
+                    <Grid
+                        container
+                        justifyContent={'center'}
+                        alignItems={'center'}
+                        sx={{
+                            position: 'fixed',
+                            bottom: '2%',
+                            left: '50%',
+                            transform: 'translateX(-50%)',
+                            width: '98%',
+                            zIndex: 1000,
+                            borderRadius: '10px',
+                            border: 'solid 1px #676767',
+                            backgroundColor: '#494949',
+                        }}
+                    >
+                        <Grid item xs={6}>
+                            <Typography variant="caption">
+                                Total Amount :
+                            </Typography>
+                            <Typography
+                                variant="h1"
+                                sx={{ fontSize: '1.5rem' }}
+                            >
+                                ₹ 0
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                size="small"
+                                onClick={handleSubmit}
+                                sx={{ ...theme.buttons.gradient }}
+                            >
+                                Submit
+                            </Button>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Box>
